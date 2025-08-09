68235fde3c9a65fc9db4042bd2f2094587b411ff
dharmsaliya <145461935+dharmsaliya@users.noreply.github.com>
2025-08-09 23:29:07 +0530

    Removed Rate limiter

--- server.js
+++ server.js
@@ -14,12 +14,12 @@ const app = express();
 app.use(helmet());
 
 // Rate limiting
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // limit each IP to 100 requests per windowMs
-  message: 'Too many requests from this IP, please try again later.'
-});
-app.use(limiter);
+// const limiter = rateLimit({
+//   windowMs: 15 * 60 * 1000, // 15 minutes
+//   max: 100, // limit each IP to 100 requests per windowMs
+//   message: 'Too many requests from this IP, please try again later.'
+// });
+// app.use(limiter);
 
 // CORS
 app.use(cors({
